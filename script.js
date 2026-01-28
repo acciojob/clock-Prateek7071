4e8aa1f187a77b31ff41f8fc74c6d2900b848860
Prateek <106758759+Prateek7071@users.noreply.github.com>
2026-01-29 01:28:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let timee = document.getElementById('timer')
+setInterval(()=>{
+	const date = new Date
+	timee.innerHTML=`date.toLocaleDateString(), date.toLocaleTimeString()`
+},100)
